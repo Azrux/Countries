66e14f801a6fd69f000da79881ef3bd921ad0503
Azrux <lausofimarce@gmail.com>
2022-10-01 20:31:23 -0300

    css fixed

--- client/src/Components/Home/Components/Home.jsx
+++ client/src/Components/Home/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import NavBar from '../Components/NavBar.jsx';
 import { getCountries } from '../../../Redux/Actions/countryActions'
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SortBy from './SortBy.jsx';
 import AlltypeCards from './AlltypeCards.jsx';
 import style from '../Styles/Home.module.css'
@@ -10,18 +10,19 @@ import style from '../Styles/Home.module.css'
 export default function Home() {
 
   const dispatch = useDispatch();
-
+  const page = useSelector(state => state.currentPage)
   useEffect(() => {
     dispatch(getCountries());
   }, [dispatch]);
 
 
   return (
-    <div className={style.background}>
+    <div >
         <NavBar />
       <div>
           <SortBy />
         <div>
+      <div className={page === 1 ? style.backgroundOne : style.background}></div>
           <AlltypeCards />
         </div>
       </div>
