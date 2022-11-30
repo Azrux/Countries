2a135ab6b925f79b3fb9adc6be7b60dd317281e8
Azrux <lausofimarce@gmail.com>
2022-11-30 19:37:25 -0300

    some details

--- client/src/Redux/Store/index.js
+++ client/src/Redux/Store/index.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, 
+  // compose 
+} from 'redux';
 import reducer from '../Reducer';
 import thunk from 'redux-thunk';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer, 
-  composeEnhancer(applyMiddleware(thunk))
+  applyMiddleware(thunk)
 )
   
 export default store;
